bfdc277bf25cbfea84b886ded7338ffaa33777f0
m.ezzat <m.ezzat@glamera.co>
2026-05-16 21:05:54 +0300

    init commit

---
+++ src/main/java/com/mezzat/recipyapp/RecipyAppApplication.java
@@ -0,0 +1,13 @@
+package com.mezzat.recipyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipyAppApplication.class, args);
+    }
+
+}
